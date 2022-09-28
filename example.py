d2e90eb2baf6a1606cd4b554c7bb66152aab173d
Simply Equipped <92337033+simplyequipped@users.noreply.github.com>
2022-09-28 14:37:18 -0400

    Add comments and clean up

--- example.py
+++ example.py
@@ -1,9 +1,11 @@
 import time
 import pyjs8call
 
+# callback for received messages
 def rx_message(msg):
     print('\t--- Message from ' + str(msg['from']) + ': ' + str(msg['text']))
 
+# callback for all new spots
 def new_spots(spots):
     for spot in spots:
         if spot['grid'] == '':
@@ -13,6 +15,7 @@ def new_spots(spots):
 
         print('\t--- Spot: ' + str(spot['from']) + grid + '@ ' + str(spot['offset']) + ' Hz\t' + time.strftime('%x %X', time.localtime(spot['time'])))
 
+# function for sending a directed message
 def send_message():
     global modem
     dest = input('\n\tEnter destination callsign: ')
@@ -20,6 +23,7 @@ def send_message():
     modem.send_directed_message(dest, msg)
     print('\n\tSending message on next transmit cycle\n')
 
+# function for showing inbox messages
 def show_inbox():
     global modem
     messages = modem.get_inbox_messages()
@@ -35,6 +39,7 @@ def show_inbox():
     else:
         print('')
 
+# function for printing the menu and handling selections
 def show_menu():
     menu =  '\n---------------  Menu  ---------------'
     menu += '\n   m)     Send message'
@@ -55,37 +60,30 @@ def show_menu():
         exit()
 
 
-
-
-#modem = pyjs8call.Modem(headless = True)
+# initialize the modem object and start the js8call application
 modem = pyjs8call.Modem()
-#modem.js8call._debug = True
+# set callback functions
 modem.set_rx_callback(rx_message)
 modem.spot_monitor.set_new_spot_callback(new_spots)
 
+# read current configuration values
 freq = modem.get_freq()
 offset = modem.get_offset()
 grid = modem.get_station_grid()
 callsign = modem.get_station_callsign()
+# check if connected to js8call
 connected = modem.js8call_connected()
 
+# parse connected state
 if connected:
     state = 'Connected'
 else:
     state = 'Disconnected'
 
+# print a summary of this station
 print('\nStation ' + callsign + ' (' + grid + ') - ' + state + ' ---------------------------------------------')
 print('Frequency: ' + str(freq / 1000000).format('0.000') + 'MHz (' + str(offset) + 'Hz)\n')
 
+# show the menu until the user exits
 while modem.online:
     show_menu()    
-
-
-
-
-
-
-
-
-
-
