66e32d53f6928ba10c5f864d2e8e592b424e22a7
OtisByron <howard@simplyequipped.com>
2022-10-27 23:11:54 -0400

    rewrite module logic

--- pyjs8call/offsetmonitor.py
+++ pyjs8call/offsetmonitor.py
@@ -9,124 +9,118 @@ class OffsetMonitor:
         self.min_offset = 1000
         self.max_offset = 2500
         self.heard_station_age = 3 * 60 # seconds
-        self.enabled = False
-
-        self.enable()
-
-    def enable(self):
-        if self.enabled:
-            return None
-
-        self.enabled = True
-        # current auto offset
+        self.bandwidth = self.client.get_bandwidth()
+        self.bandwidth_safety_factor = 1.25
         self.offset = self.client.get_offset()
-        # last auto offset
         self.previous_offset = self.offset
-        # offset before auto offset was enabled
-        self.original_offset = self.offset
+        self.enabled = True
 
+        # start monitoring thread
         monitor_thread = threading.Thread(target=self._monitor)
         monitor_thread.setDaemon(True)
         monitor_thread.start()
 
-    def disable(self):
-        if not self.enabled:
-            return None
+    def min_signal_freq(self, offset, bandwidth):
+        return offset - (bandwidth / 2)
 
-        self.enabled = False
-        # reset offset
-        self.client.set_offset(self.original_offset)
+    def max_signal_freq(self, offset, bandwidth):
+        return offset + (bandwidth / 2)
 
-    def do_change_offset(self, offset):
-        # half of the max bandwidth (160Hz @ turbo speed)
-        half_bw = 80
+    def signal_overlapping(self, offset, bandwidth):
+        other_min_freq = self.min_signal_freq(offset, bandwidth)
+        other_max_freq = self.max_signal_freq(offset, bandwidth)
+        own_min_freq   = self.min_signal_freq(self.offset, self.bandwidth)
+        own_max_freq   = self.max_signal_freq(self.offset, self.bandwidth)
 
-        # station offset equals the current offset
-        if offset == self.offset:
-            return True
-        # station overlapping from just below the current offset
-        elif offset < self.offset and (offset + half_bw) > (self.offset - half_bw):
-            return True
-        # station overlapping from just above the current offset
-        elif offset > self.offset and (offset - half_bw) < (self.offset + half_bw):
+        if (
+            # signal offset within our signal bandwidth
+            (offset > own_min_freq and offset < own_max_freq) or
+            # signal overlapping from above
+            (other_min_freq > own_min_freq and other_min_freq < own_max_freq) or
+            # signal overlapping from below
+            (other_max_freq > own_min_freq and other_max_freq < own_max_freq)
+        ):
             return True
         else:
             return False
 
-    def process_activity(self, activity):
-        offsets = [station['offset'] for station in activity]
-        offsets.sort()
-        offset_data = []
+    def parse_activity(self, activity):
+        signals = []
 
-        for i in range(len(offsets)):
-            # station offset outide min/max offset range
-            if offsets[i] < self.min_offset or offsets[i] > self.max_offset:
+        # build list of offsets and associated bandwidths
+        for spot in activity:
+            if spot['speed'] == None:
+                # assume worst case bandwidth: turbo mode
+                 signal = (spot['offset'], 160)
+            else:
+                bandwidth = self.client.get_bandwidth(speed = spot['speed'])
+                signal = (spot['offset'], bandwidth)
+
+            signals.append(signal)
+
+        signals.sort(key = lambda signal: signal[0])
+        return signals
+
+    def find_unused_spectrum(self, signals):
+        unused_spectrum = []
+
+        for i in range(len(signals)):
+            min_signal_freq = self.min_signal_freq(*signals[i])
+            max_signal_freq = self.max_signal_freq(*signals[i])
+
+            # signal outside min/max offset range
+            if max_signal_freq < self.min_offset or min_signal_freq > self.max_offset:
                 continue
 
+            # handle first signal in list
             if i == 0:
-                previous_offset = self.min_offset
+                # use minimum offset as lower edge of unused section
+                lower_limit = self.min_offset
             else:
-                previous_offset = offsets[i-1]
+                # use previous signal's upper edge as lower edge of unused section
+                lower_limit = self.get_max_signal_freq(*signals[i-1])
 
-            if i == len(offsets) - 1:
-                next_offset = self.max_offset
+            # handle last signal in list
+            if i == len(signals) - 1:
+                # use maximum offset as upper edge of unused section
+                upper_limit = self.max_offset
             else:
-                next_offset = offsets[i+1]
+                # use next signal's lower edge as upper edge of unused section
+                upper_limit = self.get_min_signal_freq(*signals[i+1])
 
-            distance = offsets[i] - self.offset
-            offset = offsets[i]
+            # unused section is wide enough for current speed setting plus safety factor
+            if (upper_limit - lower_limit) >= (self.bandwidth * self.bandwidth_safety_factor):
+                unused_spectrum.append((lower_limit, upper_limit))
 
-            data = {
-                'offset' : offset,
-                'distance' : distance,
-                'previous' : previous_offset,
-                'next' : next_offset,
-            }
+        return unused_spectrum
 
-            offset_data.append(data)
+    def find_new_offset(self, unused_spectrum):
+        # calculate distance from the current offset to each unused section
+        for i in range(len(unused_spectrum)):
+            if upper_limit < self.offset:
+                # below the current offset
+                unused_spectrum[i] += (self.offset - upper_limit, )
+            elif lower_limit > self.offset:
+                # above the current offset
+                unused_spectrum[i] += (lower_limit - self.offset, )
 
-        offset_data.sort(key=lambda offset: abs(offset['distance']))
+        # sort by distance from current offset
+        unused_spectrum.sort(key = lambda section: section[2])
 
-        return offset_data
+        # use nearest unused section
+        lower_limit = unused_spectrum[0][0]
+        upper_limit = unused_spectrum[0][1]
 
-    def find_offset_data(self, offset, offsets):
-        for o in offsets:
-            if o['offset'] == offset:
-                return o
+        # move offset up the spectrum to the beginning of the next unused section
+        if lower_limit > self.offset:
+            return lower_limit + ((self.bandwidth * self.bandwidth_safety_factor) / 2)
 
-    def search_up(self, offset, offsets):
-        # find the next offset pair with at least 200Hz in betweeen
-        # 160Hz (max bandwidth in turbo mode) * 125% = 200Hz
-        offset_data = self.find_offset_data(offset)
-        next_offset = offset_data['next']
+        # move offset down the spectrum to the end of the next unused section
+        elif upper_limit < self.offset:
+            return upper_limit - ((self.bandwidth * self.bandwidth_safety_factor) / 2)
 
-        while (next_offset - offset) < 200 and next_offset < self.max_offset:
-            offset_data = self.find_offset_data(next_offset)
-            next_offset = offset_data['next']
-
-        # quit searching if the last offset pair doesn't suit either
-        if (next_offset - offset) < 200 and next_offset < self.max_offset:
+        else:
             return None
-        
-        # return the next available offset
-        return offset_data['offset'] + 200
-            
-    def search_down(self, offset, offsets):
-        # find the next offset pair with at least 200Hz in betweeen
-        # 160Hz (max bandwidth in turbo mode) * 125% = 200Hz
-        offset_data = self.find_offset_data(offset)
-        previous_offset = offset_data['previous']
-
-        while (offset - previous_offset) < 200 and previous_offset > self.min_offset:
-            offset_data = self.find_offset_data(previous_offset)
-            previous_offset = offset_data['previous']
-
-        # quit searching if the last offset pair doesn't suit either
-        if (offset - previous_offset) < 200 and previous_offset > self.min_offset:
-            return None
-        
-        # return the next available offset
-        return offset_data['offset'] - 200
             
     def _monitor(self):
         while self.client.online and self.enabled:
@@ -139,9 +133,6 @@ class OffsetMonitor:
 
             time.sleep(delay)
 
-            if not self.enabled:
-                break
-
             # get recent spots
             timestamp = time.time() - self.heard_station_age
             activity = self.client.get_station_spots(since_timestamp = timestamp) 
@@ -149,41 +140,21 @@ class OffsetMonitor:
             if len(activity) == 0:
                 continue
 
-            offsets = self.process_activity(activity)
-            new_offset = None
+            signals = self.parse_activity(activity)
+            self.bandwidth = self.client.get_bandwidth()
+            overlap = False
 
-            for offset in offsets:
-                if self.do_change_offset(offset['offset']):
-                    # first search the opposite direction of the last offset change
-                    if self.previous_offset > self.offset:
-                        # search up for a free offset
-                        new_offset = search_up(offset, offsets)
+            for signal in signals:
+                if self.signal_overlapping(*signal):
+                    overlap = True
+                    break
 
-                        # search up failed, search down for a free offset
-                        if new_offset == None:
-                            new_offset = search_down(offset, offsets)
+            if overlap:
+                unused_spectrum = self.find_unused_spectrum(signals)
+                new_offset = self.find_new_offset(unused_spectrum)
 
-                        # searching up and down both failed, abort offset change
-                        if new_offset == None:
-                            break
-                    
-                    # first search the opposite direction of the last offset change
-                    elif self.previous_offset < self.offset:
-                        # search down for a free offset
-                        new_offset = search_down(offset, offsets)
-
-                        # search down failed, search up for a free offset
-                        if new_offset == None:
-                            new_offset = search_up(offset, offsets)
-
-                        # searching up and down both failed, abort offset change
-                        if new_offset == None:
-                            break
-
-            if new_offset != None:
-                self.previous_offset = self.offset
-                self.offset = new_offset
-
-                if self.enabled:
+                if new_offset != None:
+                    self.previous_offset = self.offset
+                    self.offset = new_offset
                     self.client.set_offset(self.offset)
 
