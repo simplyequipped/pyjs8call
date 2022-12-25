c7f5cee681d9aecc1642b31a94f872ea392a4935
OtisByron <howard@simplyequipped.com>
2022-12-25 16:34:36 -0500

    update internal variables for consistency

--- pyjs8call/txmonitor.py
+++ pyjs8call/txmonitor.py
@@ -7,11 +7,11 @@ from pyjs8call import Message
 
 class TxMonitor:
     def __init__(self, client):
-        self.client = client
-        self.msg_queue = []
-        self.msg_queue_lock = threading.Lock()
+        self._client = client
+        self._msg_queue = []
+        self._msg_queue_lock = threading.Lock()
         # initialize msg max age to 30 tx cycles in fast mode (10 sec cycles)
-        self.msg_max_age = 10 * 30 # 5 minutes
+        self._msg_max_age = 10 * 30 # 5 minutes
         self.status_change_callback = None
         #self.tx_complete_callback = None
         #self.tx_failed_callback = None
@@ -32,14 +32,14 @@ class TxMonitor:
     def monitor(self, msg):
         msg.status = Message.STATUS_QUEUED
 
-        self.msg_queue_lock.acquire()
-        self.msg_queue.append(msg)
-        self.msg_queue_lock.release()
+        self._msg_queue_lock.acquire()
+        self._msg_queue.append(msg)
+        self._msg_queue_lock.release()
 
     def _monitor(self):
-        while self.client.online:
+        while self._client.online:
             time.sleep(1)
-            tx_text = self.client.get_tx_text()
+            tx_text = self._client.get_tx_text()
 
             # no text in tx field, nothing to process
             if tx_text == None:
@@ -55,14 +55,14 @@ class TxMonitor:
             #    5 min in fast mode (10 sec cycles)
             #    7.5 min in normal mode (15 sec cycles)
             #    15 min in slow mode (30 sec cycles)
-            tx_window = self.client.get_tx_window_duration()
-            self.msg_max_age = tx_window * 30
+            tx_window = self._client.get_tx_window_duration()
+            self._msg_max_age = tx_window * 30
             
-            self.msg_queue_lock.acquire()
+            self._msg_queue_lock.acquire()
 
             # process msg queue
-            for i in range(len(self.msg_queue)):
-                msg = self.msg_queue.pop(0)
+            for i in range(len(self._msg_queue)):
+                msg = self._msg_queue.pop(0)
                 msg_value = msg.destination + '  ' + msg.value.strip()
                 drop = False
 
@@ -82,7 +82,7 @@ class TxMonitor:
                         
                     drop = True
                        
-                elif time.time() > msg.timestamp + self.msg_max_age:
+                elif time.time() > msg.timestamp + self._msg_max_age:
                     # msg sending failed
                     msg.status = Message.STATUS_FAILED
 
@@ -92,7 +92,7 @@ class TxMonitor:
                     drop = True
 
                 if not drop:
-                    self.msg_queue.append(msg)
+                    self._msg_queue.append(msg)
                         
-            self.msg_queue_lock.release()
-            
+            self._msg_queue_lock.release()
+           
--- pyjs8call/windowmonitor.py
+++ pyjs8call/windowmonitor.py
@@ -5,52 +5,51 @@ import pyjs8call
 
 class WindowMonitor:
     def __init__(self, client):
-        self.client = client
-        self.last_tx_frame_timestamp = 0
-        self.next_window_timestamp = 0
-        self.window_duration = self.client.get_tx_window_duration()
-        self.window_callback = None
-        self.locked = False
+        self._client = client
+        self._last_tx_frame_timestamp = 0
+        self._next_window_timestamp = 0
+        self._window_duration = self._client.get_tx_window_duration()
+        self._window_callback = None
 
         monitor_thread = threading.Thread(target = self._monitor)
         monitor_thread.setDaemon(True)
         monitor_thread.start()
 
     def set_window_callback(self, callback):
-        self.window_callback = callback
+        self._window_callback = callback
 
     def process_tx_frame(self, msg):
-        self.last_tx_frame_timestamp = msg.time
+        self._last_tx_frame_timestamp = msg.timestamp
 
     def next_window_start(self):
-        if self.last_tx_frame_timestamp == 0:
+        if self._last_tx_frame_timestamp == 0:
             return 0
 
-        return self.next_window_timestamp
+        return self._next_window_timestamp
 
     def next_window_end(self):
-        if self.last_tx_frame_timestamp == 0:
+        if self._last_tx_frame_timestamp == 0:
             return 0
 
-        return self.next_window_timestamp + self.window_duration
+        return self._next_window_timestamp + self._window_duration
 
     def _monitor(self):
-        while self.client.online:
+        while self._client.online:
             # wait for first tx frame
-            if self.last_tx_frame_timestamp == 0:
+            if self._last_tx_frame_timestamp == 0:
                 pass
 
             # within 1 millisecond of window start
-            elif self.next_window_timestamp < time.time():
-                if self.window_callback != None:
-                    self.window_callback()
+            elif self._next_window_timestamp < time.time():
+                if self._window_callback != None:
+                    self._window_callback()
 
-                self.window_duration = self.client.get_tx_window_duration()
+                self._window_duration = self._client.get_tx_window_duration()
 
-                if self.next_window_timestamp == 0:
-                    self.next_window_timestamp = self.last_tx_frame_timestamp + self.window_duration
+                if self._next_window_timestamp == 0:
+                    self._next_window_timestamp = self._last_tx_frame_timestamp + self._window_duration
                 else:
-                    self.next_window_timestamp += self.window_duration
+                    self._next_window_timestamp += self._window_duration
 
             time.sleep(0.001)
 
