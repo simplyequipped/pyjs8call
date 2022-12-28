b238593be93fd9aefcd3cd9d2776e29018f1685d
OtisByron <howard@simplyequipped.com>
2022-12-27 21:46:01 -0500

    add docs

--- pyjs8call/spot.py
+++ pyjs8call/spot.py
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+'''Spot object for spotting heard stations.'''
+
 __docformat__ = 'google'
 
 
@@ -27,7 +29,31 @@ import time
 
 
 class Spot:
+    '''Spot object for spotting heard stations.
+
+    A station spot is constructed from a pyjs8call.message and copies attributes from the message.
+
+    Attributes:
+        msg_id (str): ID of the message
+        origin (str): Origin callsign of the message
+        destination (str): Destination callsign of the message
+        freq (int): Dial frequency of the message in Hz
+        offset (int): Offset frequency of the message in Hz
+        time (float): UTC timestamp of the message
+        timestamp (float): Local timestamp of the message
+        grid (str): Grid square of the message
+        snr (int): Signal-to-noise ratio of the message
+        speed (str): JS8Call modem speed of the messsage
+    '''
     def __init__(self, msg):
+        '''Initialize spot.
+
+        Args:
+            msg (pyjs8call.message): Message used to construct the spot
+
+        Returns:
+            pyjs8call.spot: Constructed spot object
+        '''
         self.msg_id = msg.id
         self.origin = msg.origin
         self.destination = msg.destination
@@ -40,6 +66,18 @@ class Spot:
         self.speed = msg.speed
 
     def __eq__(self, spot):
+        '''Whether another spot is considered equal to self.
+
+        There are two cases where spots are considered equal:
+        - When both spots have the same UTC timestamps (literally the same spot)
+        - When both spots have the same origin, offset frequency, and snr (same station event reported by differnt JS8Call API messages at slightly differnt times) 
+
+        Args:
+            spot (pyjs8call.spot): Spot to compare
+
+        Returns:
+            bool: Whether the two spots are considered equal
+        '''
         # comparing origin, offset, and snr allows equating the same message sent more than once
         # from the js8call application (likely as different message types) at slightly different
         # times (milliseconds apart)
@@ -52,11 +90,36 @@ class Spot:
             return False
 
     def __lt__(self, spot):
+        '''Whether another spot is less than self.
+
+        UTC timestamps are compared.
+
+        Args:
+            spot (pyjs8call.spot): Spot to compare
+
+        Returns:
+            bool: Whether self.time is less than the specified spot.time 
+        '''
         return bool(self.time < spot.time)
 
     def __gt__(self, spot):
+        '''Whether another spot is greater than self.
+
+        UTC timestamps are compared.
+
+        Args:
+            spot (pyjs8call.spot): Spot to compare
+
+        Returns:
+            bool: Whether self.time is greater than the specified spot.time 
+        '''
         return bool(self.time > spot.time)
 
     def age(self):
+        '''Age of the spot.
+
+        Returns:
+            float: Spot age in seconds
+        '''
         return time.time() - self.timestamp
 
