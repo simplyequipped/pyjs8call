190b1923b9805a00eef4612cf38cd8820ec6c706
Simply Equipped <92337033+simplyequipped@users.noreply.github.com>
2023-08-14 16:01:52 -0400

    use incoming rx.activity instead of spots

--- pyjs8call/offsetmonitor.py
+++ pyjs8call/offsetmonitor.py
@@ -24,7 +24,7 @@
 
 The offset frequency is automatically moved to an unused portion of the pass band if a recently heard signal overlaps with the current offset. Signal bandwidth is calculated based on the speed of each heard signal.
 
-Only decoded signal data is available from the JS8Call API so other QRM cannot be handled.
+Only decoded signal data is available from the JS8Call API, so other QRM cannot be handled.
 '''
 
 __docformat__ = 'google'
@@ -44,7 +44,7 @@ class OffsetMonitor:
         bandwidth_safety_factor (float): Safety factor to apply around outgoing signal bandwith, defaults to 1.25
         offset (int): Current JS8Call offset frequency in Hz
         before_transition (int, float): Seconds before the rx/tx window transition to process activity, defaults to 1
-        activity_cycles (int, float): rx/tx cycles to consider recent activity, defaults to 1.5
+        activity_cycles (int, float): rx/tx cycles to consider recent activity, defaults to 2.5
     '''
 
     def __init__(self, client):
@@ -63,10 +63,13 @@ class OffsetMonitor:
         self.bandwidth_safety_factor = 1.25
         self.offset = self._client.settings.get_offset()
         self.before_transition = 1
-        self.activity_cycles = 1.5
+        self.activity_cycles = 2.5
         self._enabled = False
         self._paused = False
         self._hb = False
+
+        self._recent_signals = []
+        self._recent_signals_lock = threading.Lock()
 
     def enabled(self):
         '''Get enabled status.
@@ -90,6 +93,7 @@ class OffsetMonitor:
             return
 
         self._enabled = True
+        self._client.callback.register_incoming(self.process_rx_activity, message_type = Message.RX_ACTIVITY)
 
         thread = threading.Thread(target=self._monitor)
         thread.daemon = True
@@ -98,6 +102,7 @@ class OffsetMonitor:
     def disable(self):
         '''Disable offset monitoring.'''
         self._enabled = False
+        self._client.callback.remove_incoming(self.process_rx_activity)
 
     def pause(self):
         '''Pause offset monitoring.'''
@@ -106,6 +111,31 @@ class OffsetMonitor:
     def resume(self):
         '''Resume offset monitoring.'''
         self._paused = False
+
+    def process_rx_activity(self, activity):
+        '''Process recent incoming activity.
+
+        Note: This function is called internally when activity is received.
+
+        Args:
+            object (pyjs8call.Message): RX.ACTIVITY message from JS8Call
+        '''
+        # ignore activity outside the specified pass band
+        if activity.offset < self.min_offset or activity.offset > self.max_offset:
+            return
+            
+        if activity.speed is None:
+            # assume worst case bandwidth: turbo mode = 160 Hz
+            signal = (activity.offset, 160, activity.timestamp)
+        else:
+            # map signal speed to signal bandwidth
+            bandwidth = self._client.settings.get_bandwidth(speed = activity.speed)
+            signal = (activity.offset, bandwidth, activity.timestamp)
+
+        with self._recent_signals_lock:
+            self._recent_signals.append(signal)
+            # sort signals in ascending order by offset
+            self._recent_signals.sort(key = lambda signal: signal[0])
 
     def _min_signal_freq(self, offset, bandwidth):
         '''Get lower edge of signal.
@@ -132,12 +162,31 @@ class OffsetMonitor:
         '''
         return int(offset + bandwidth)
 
-    def signal_overlapping(self, offset, bandwidth):
+    def _activity_overlapping(self, activity):
+        '''Check for received signals overlapping station transmit region.
+
+        Args:
+            activity (list): Recent activity signal data
+
+        Returns:
+            bool: True if a recently received signal is overlapping, False otherwise
+        '''
+        if len(activity) == 0:
+            return False
+        
+        for signal in activity:
+            if self._signal_overlapping(*signal):
+                return True
+
+        return False
+
+    def _signal_overlapping(self, offset, bandwidth, timestamp):
         '''Determine if signal overlaps with current offset.
 
         Args:
             offset (int): Signal offset frequency in Hz
             bandwidth (int): Signal bandwidth in Hz
+            timestamp (float): Received timestamp in seconds
 
         Returns:
             bool: Whether the given signal overlaps with the transmit signal space
@@ -158,36 +207,7 @@ class OffsetMonitor:
 
         return any([inside, above, below])
 
-    def parse_activity(self, activity):
-        '''Parse recent activity into signal data.
-
-        Recent spot data is processed to generate tuples of the following structure: (offset, bandwidth). The returned list of tuples is sorted by in ascending order by offset frequency.
-
-        Args:
-            activity (list): List of recent spots to parse
-
-        Returns:
-            list: List of signal tuples
-        '''
-        signals = []
-
-        # build list of offsets and associated bandwidths
-        for spot in activity:
-            if spot.speed is None:
-                # assume worst case bandwidth: turbo mode = 160 Hz
-                signal = (spot.offset, 160)
-            else:
-                # map signal speed to signal bandwidth
-                bandwidth = self._client.settings.get_bandwidth(speed = spot.speed)
-                signal = (spot.offset, bandwidth)
-
-            signals.append(signal)
-        
-        # sort signals in ascending order by offset
-        signals.sort(key = lambda signal: signal[0])
-        return signals
-
-    def find_unused_spectrum(self, signals):
+    def _find_unused_spectrum(self, signals):
         '''Find available pass band sections.
 
         Available sections of the pass band are sections that are wide enough for a transmitted signal based on the configured bandwidth (i.e. configured JS8Call modem speed) plus a safety margin. The returned tuples represent the lower and upper limits of available pass band sections.
@@ -269,17 +289,24 @@ class OffsetMonitor:
 
         return unused_spectrum
 
-    def find_new_offset(self, unused_spectrum):
+    def _find_new_offset(self, activity):
         '''Get new offset frequency.
 
-        Find a new offset based on available sections in the pass band. The new offset is always moved to the next closed available section of the pass band.
+        Find a new offset based on available sections in the pass band. The new offset is always moved to the closed available section of the pass band.
 
         Args:
-            unused_spectrum (list): List of tuples of available sections
+            signals (list): List of signal tuples
 
         Returns:
-            int or None: New offset frequency in Hz, or None if there are no available sections
+            int: New offset frequency in Hz
+            None: no unused specturm is available
         '''
+        # find unused spectrum (between heard signals)
+        unused_spectrum = self._find_unused_spectrum(activity)
+
+        if len(unused_spectrum) == 0:
+            return None
+            
         # calculate distance from the current offset to each unused section
         distance = []
 
@@ -320,7 +347,30 @@ class OffsetMonitor:
             return int(upper_limit - safe_bandwidth)
         else:
             return None
-            
+
+    def _cull_recent_activity(self):
+        '''Remove aged signal activity.
+
+        Must be called from within self._recent_activity_lock context.
+        '''
+        recent_activity = []
+        offsets = []
+        max_age = int(self.activity_cycles * self._client.settings.get_window_duration())
+        
+        # sort recent signals in descending order by timestamp,
+        # causes the most recent activity on the same offset to be kept while culling
+        self._recent_activity.sort(key = lambda signal: signal[2], reverse = True)
+        
+        now = time.time()
+        
+        for signal in self._recent_activity:
+            if signal[0] not in offsets and now - signal[2] <= max_age:
+                # keep recent signals with a unique offset
+                recent_activity.append(signal)
+                offsets.append(signal[0])
+                
+        self._recent_activity = recent_activity
+    
     def _monitor(self):
         '''Offset monitor thread.
 
@@ -351,28 +401,11 @@ class OffsetMonitor:
                     # middle of pass band
                     self.offset = ((self.max_offset - self.min_offset) / 2) + self.min_offset
 
-            # get recent spots
-            activity_age = int(self.activity_cycles * self._client.settings.get_window_duration())
-            activity = self._client.spots.filter(age = activity_age) 
-
-            if len(activity) > 0:
-                # process activity into signal tuples (min_freq, max_freq)
-                signals = self.parse_activity(activity)
-
-                # check for signals overlapping our signal
-                overlap = False
-                for signal in signals:
-                    if self.signal_overlapping(*signal):
-                        overlap = True
-                        break
-
-                if overlap:
-                    # find unused spectrum (between heard signals)
-                    unused_spectrum = self.find_unused_spectrum(signals)
-
-                    if len(unused_spectrum) > 0:
-                        # find nearest unused spectrum and determine new offset
-                        new_offset = self.find_new_offset(unused_spectrum)
+            with self._recent_activity_lock:
+                self._cull_recent_activity()
+                # check for signal overlap with transmit region
+                if self._activity_overlapping(self._recent_activity):
+                    new_offset = self._find_new_offset(self._recent_activity)
 
             # set new offset
             if new_offset is not None:
