02dec8831abd882803711222d4bd394cf9f5af0f
Simply Equipped <92337033+simplyequipped@users.noreply.github.com>
2024-02-14 14:52:22 -0500

    update version, add lxmf requirement

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='pyjs8call',
-    version='0.2.3',
+    version='0.3.0',
     author='Simply Equipped LLC',
     author_email='howard@simplyequipped.com',
     description='Python package for interfacing with the JS8Call API',
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/simplyequipped/pyjs8call',
     packages=setuptools.find_packages(),
-    install_requires=['psutil>=5.3'],
+    install_requires=['psutil>=5.3', 'lxmf>=0.3.9'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
